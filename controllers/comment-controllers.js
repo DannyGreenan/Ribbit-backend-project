97b0c77ed155c8f1754b3f65d29295bf46139ac1
Danny <danny.greenan@live.co.uk>
2024-08-30 09:42:09 +0100

    Completed task 17 added /api/comments/:comment_id endpoint with PATCH and GET requests

--- controllers/comment-controllers.js
+++ controllers/comment-controllers.js
@@ -1,10 +1,42 @@
-const { deleteCommentById } = require("../models/comment-models");
+const {
+  deleteCommentById,
+  patchCommentVotesById,
+  getCommentById,
+} = require("../models/comment-models");
+
+exports.getComment = (req, res, next) => {
+  const { comment_id } = req.params;
+
+  getCommentById(comment_id)
+    .then((comment) => {
+      res.status(200).send({ comment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.deleteComment = (req, res, next) => {
   const { comment_id } = req.params;
   deleteCommentById(comment_id)
     .then((msg) => {
       res.sendStatus(204);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchCommentVotes = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { comment_id } = req.params;
+
+  getCommentById(comment_id)
+    .then(() => {
+      return patchCommentVotesById(comment_id, inc_votes);
+    })
+    .then((comment) => {
+      res.status(201).send({ comment });
     })
     .catch((err) => {
       next(err);
--- routers/comment-routers.js
+++ routers/comment-routers.js
@@ -2,12 +2,16 @@ const express = require("express");
 const {
   deleteComment,
   handleInvalidMethod,
+  patchCommentVotes,
+  getComment,
 } = require("../controllers/comment-controllers");
 const router = express.Router();
 
 router
   .route("/api/comments/:comment_id")
+  .get(getComment)
   .delete(deleteComment)
+  .patch(patchCommentVotes)
   .all(handleInvalidMethod);
 
 module.exports = router;
